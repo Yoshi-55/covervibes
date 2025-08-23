2750747a8dc958158453eda748f4b98b5c3ace5a
Yoshi-55 <ysmr9855@gmail.com>
2025-08-23 13:03:58 +0900

    firstcommit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/assets/stylesheets/**/*.css'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
