94eb843f0325889520e4f59e38efa8aad6a1f244
Yoshi-55 <ysmr9855@gmail.com>
2025-08-25 11:35:34 +0900

    シェア時のtrackのメタタグを表示できるように修正

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,5 +8,22 @@ class SongsController < ApplicationController
     else
       @tracks = service.random_search(32, market)
     end
+    if params[:trackid].present?
+      begin
+        track = RSpotify::Track.find(params[:trackid])
+        @tracks = track ? [track] : []
+        if track
+          @track_meta = {
+            title: track.name,
+            artist: track.artists.map(&:name).join(', '),
+            image: track.album&.images&.first&.dig('url'),
+            url: request.original_url
+          }
+        end
+      rescue => e
+        Rails.logger.error "Track fetch error: #{e.message}"
+        @tracks = []
+      end
+    end
   end
 end
