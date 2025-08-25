fb8441dfc36dea1977163ff703cd4f0416832319
Yoshi-55 <ysmr9855@gmail.com>
2025-08-25 11:07:39 +0900

    Link先の変更

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,15 @@
 class SongsController < ApplicationController
   def index
     service = SpotifySearchService.new
-    if params[:genre].present? && GenreHelper.valid_genre?(params[:genre])
+    if params[:trackid].present?
+      begin
+        track = RSpotify::Track.find(params[:trackid])
+        @tracks = track ? [track] : []
+      rescue => e
+        Rails.logger.error "Track fetch error: #{e.message}"
+        @tracks = []
+      end
+    elsif params[:genre].present? && GenreHelper.valid_genre?(params[:genre])
       @tracks = service.genre_search(params[:genre], 32)
     else
       @tracks = service.random_search(32)
