5731e8ce26d250801357f3e40a13c45167291696
Yoshi-55 <ysmr9855@gmail.com>
2025-08-25 13:01:46 +0900

    lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/healthz', to: proc { [200, {}, ['ok']] }
+  get "/healthz", to: proc { [ 200, {}, [ "ok" ] ] }
   get "songs", to: "songs#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
